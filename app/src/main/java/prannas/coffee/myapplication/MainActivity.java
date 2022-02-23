576d4e32d0fba44a0c7579749f657f74018febe6
PravardhaSagar <pravardha.u@gmail.com>
2022-02-23 23:37:47 +0530

    comments added

--- app/src/main/java/prannas/coffee/myapplication/MainActivity.java
+++ app/src/main/java/prannas/coffee/myapplication/MainActivity.java
@@ -1,31 +1,51 @@
 package prannas.coffee.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    List<String[]> Cred= new ArrayList<String[]>();
+    String[][] cred = new String[10][2];
+    int x = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button Login=(Button)findViewById(R.id.button);
-        EditText Username=(EditText)findViewById((R.id.username));
-        EditText Password=(EditText)findViewById((R.id.password));
+        Button Login = (Button) findViewById(R.id.button);
+        EditText Username = (EditText) findViewById((R.id.username));
+        EditText Password = (EditText) findViewById((R.id.password));
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] s=new String[2];
-                s[0]=Username.getText().toString();
-                s[1]=Password.getText().toString();
-                Cred.add(s);
+//                Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
+
+                String[][] cred={{"vic", "pre"}, {"aru", "pra"}, {"pra", "aru"}, {"google", "android"}};
+                String s,j;
+                s=Username.getText().toString();
+                j=Password.getText().toString();
+                Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+                int c=0;
+                //this
+                //is
+                //just
+                //for branch
+                for(int i=0;i<4;i++)
+                {
+                    if(cred[i][0].equals(s) && cred[i][1].equals(j)){
+                        Toast.makeText(getApplicationContext(), "Successful", Toast.LENGTH_SHORT).show();
+                        c=1;
+                        break;
+                    }
+                }
+                if(c==0){
+                    Toast.makeText(getApplicationContext(), "Unsucessfull", Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
